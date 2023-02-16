4fca85a8152a9299a88e8bd708e5a673530de7dd
njeri-max <njeriwan8@gmail.com>
2023-02-16 13:25:30 -0800

    size types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
-
-
-
-
-
-
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0 if successful
+ */
+int main(void)
 {
-
-	printf("Char size: %lu bytes\n", sizeof(char));
-	printf("Int size: %lu bytes\n", sizeof(int)); 
-	printf("Short size: %lu byte\n", sizeof(short));
-	printf("Long size: %lu byte(s)\n", sizeof(long));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
+
