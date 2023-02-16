a3566b8d144db4aa43fc4f2e51ea6a766cf0ffeb
njeri-max <njeriwan8@gmail.com>
2023-02-16 03:02:39 -0800

    output file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 #include <stdio.h>
 /**
  * main - main block
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
