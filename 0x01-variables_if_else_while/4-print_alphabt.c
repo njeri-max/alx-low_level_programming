34661dfd969787b7c0945592091158692ec469a2
njeri-max <njeriwan8@gmail.com>
2023-02-17 12:10:10 -0800

    printing alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-*main - prints the alphabet in lowercase, excempting q and e
+ * main - prints the alphabet in lowercase, excempting q and e
 (*
  * Return 0 on success
-*/
+ */
 int main(void)
 {
 	char i = 'a';
