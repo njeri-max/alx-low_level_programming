ab853ff4d97ca4730a0b33c3d6b4739b697c9a6b
njeri-max <njeriwan8@gmail.com>
2023-02-17 12:07:13 -0800

    Alphabet with exemption

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - prints the alphabet in lowercase, excempting q and e
+(*
+ * Return 0 on success
+*/
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
