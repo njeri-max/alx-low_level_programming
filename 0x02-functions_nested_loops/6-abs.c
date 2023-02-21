d9dcd3f4285d646b726cc7f1482be126258cd4b0
njeri-max <njeriwan8@gmail.com>
2023-02-21 07:08:31 -0800

    absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - calculate absolute value of number from 0
+ * @j: int type
+ * Return: absolute value of @j
+ */
+int _abs(int j)
+{
+	if (j < 0)
+	{
+		return (j * -1);
+	}
+	else
+	{
+		return (j);
+	}
+}
+
