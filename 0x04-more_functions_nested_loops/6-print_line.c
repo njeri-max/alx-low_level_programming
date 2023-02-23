ec705a3c8a5e6fd08b02fb5704d775981f2cf7c6
njeri-max <njeriwan8@gmail.com>
2023-02-23 11:23:54 -0800

    straight lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * void print_line - function that draws a straight line in the terminal
+ * print_line - function that draws a straight line in the terminal
  * @n: the number of times '_' is printed
  * Description: only use  _putchar to print
  * Return a straight line ending with \n
