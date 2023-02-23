0b5a75f57f91ccdbd23b578285ac0889f304202c
njeri-max <njeriwan8@gmail.com>
2023-02-23 08:32:25 -0800

    isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - the function that checks for uppercase letters
+ * @c: is the int type number
+ * Return: 1 if c is uppercase, otherwise return 0
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 64)
+		{return (1); }
+	else
+		{return (0); }
+}
