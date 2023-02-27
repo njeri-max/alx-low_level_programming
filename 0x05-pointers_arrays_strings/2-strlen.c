e1eb20d0343bf8d0a8bbecd5017ab0a02e027c1d
njeri-max <njeriwan8@gmail.com>
2023-02-27 03:47:17 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function that returns length of a string
+ * @s: char type pointer
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int z;
+
+	for (z = 0; s[z] != '\0'; z++)
+	{
+		;
+	}
+	return (z);
+}
+
