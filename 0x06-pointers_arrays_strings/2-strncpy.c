51ccdbb90eb3520c7664d61cd0531daeb09ae64a
njeri-max <njeriwan8@gmail.com>
2023-03-01 22:46:55 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,27 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a;
+	int a = 0, b = 0;
 
-	for (a = 0; a < n; a++)
+	while (src[b])
+	{
+		b++;
+	}
+
+	while (a < n && src[a])
 	{
 		dest[a] = src[a];
+		a++;
 	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
 	return (dest);
-}
+} 
+
 
 
