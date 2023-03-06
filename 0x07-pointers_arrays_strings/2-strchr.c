076e833b2e757cca4099703ede4711d7f0f7aa98
njeri-max <njeriwan8@gmail.com>
2023-03-06 05:07:21 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - Locates a character in a  string
+ * @c: character type
+ * @s: Charcter pointer type
+ * Return: pointer to the first occurence of a character
+ * or NULL if it is not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else if (*(s + 1) == c)
+		{
+			return (s + 1);
+		}
+		s++;
+	}	
+	return (s + 1);
+}
