e2514a7d4590859acafaa7b33df09aa503dcc533
njeri-max <njeriwan8@gmail.com>
2023-03-06 05:22:47 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: character pointer type
+ * @accept: char array to compare bytes
+ * Returns: number of bytes in initial segment `s`
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int c;
+
+	i = 0;
+	c = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+				break;
+			}
+			j++;
+		}
+		if (accept[j] == '\0')
+			break;
+		i++;
+	}
+	return (c);
+}
