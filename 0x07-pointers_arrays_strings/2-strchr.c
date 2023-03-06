c1535b9e8a364d3efb3c64587054782fc2f5bb09
njeri-max <njeriwan8@gmail.com>
2023-03-06 05:11:19 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,11 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
 		else if (*(s + 1) == c)
-		{
 			return (s + 1);
-		}
 		s++;
-	}	
+	}
+
 	return (s + 1);
 }
