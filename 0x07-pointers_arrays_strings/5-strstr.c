00d1a6455662b497925a5ead06cef7e643a18fdc
njeri-max <njeriwan8@gmail.com>
2023-03-06 10:09:56 -0800

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring through identyfying `needle`
+ * in `haystack`
+ * @haystack: string
+ * @needle: string substring
+ * Return: pointer to the substring and NULL if not located
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a, b, c;
+
+	a = 0;
+	c = 0;
+	while (haystack[a] != '\0')
+	{
+		b = 0;
+		while ((needle[b + c] != '\0' && haystack[a + c] != '\0')
+		 && (needle[b + c] == haystack[a + c]))
+		{
+			if (haystack[a + c] != needle[b + c])
+				break;
+			c++;
+		}
+		if (needle[b + c] != '\0')
+			return (&haystack[a]);
+		b++;
+		a++;
+	}
+	return (NULL);
+}
