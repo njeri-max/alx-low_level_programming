b277232c6192c465fd9ce3be202a76c26a6d93fd
njeri-max <njeriwan8@gmail.com>
2023-03-07 23:09:11 -0800

    number 2 length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int f;
+
+	f = 0;
+	if (*s == '\0')
+		return (0);
+
+	f  = 1 + _strlen_recursion(s + 1);
+
+	return (f);
+}
+
+
